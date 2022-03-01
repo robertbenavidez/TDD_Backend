76ca2aa3f3972c69d4cfd14459ce7dd57cbb1ff3
Robert Benavidez <murabitbenavidez@gmail.com>
2022-03-01 13:49:39 -0500

    updated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 // create post handler
 app.post('/api/1.0/users', (req, res) => {
-  return res.send({message: "User created"});
+  return res.send({ message: 'User created' });
 });
 
 module.exports = app;
