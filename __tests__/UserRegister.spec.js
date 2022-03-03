fc36faade2f83ad545460770094850fee802719e
Robert Benavidez <murabitbenavidez@gmail.com>
2022-03-02 23:49:46 -0500

    username email and password saved to the database

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -1,5 +1,16 @@
 const request = require('supertest');
 const app = require('../src/app');
+const User = require('../src/user/User');
+const sequelize = require('../src/config/database');
+
+// initializes database
+beforeAll(() => {
+  return sequelize.sync();
+});
+// cleans up database before each test
+beforeEach(() => {
+  return User.destroy({ truncate: true });
+});
 
 describe('User Registration', () => {
   it('returns 200 Ok when signup request is valid', (done) => {
@@ -27,6 +38,24 @@ describe('User Registration', () => {
       .then((response) => {
         expect(response.body.message).toBe('User created');
         done();
+      });
+  });
+  it('saves the username, email and password to the database', (done) => {
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username: 'user1',
+        email: 'user1@mail.com',
+        password: 'p4ssword',
+      })
+      .then(() => {
+        // query user table
+        User.findAll().then((userList) => {
+          const savedUser = userList[0];
+          expect(savedUser.username).toBe('user1');
+          expect(savedUser.email).toBe('user1@mail.com');
+          done();
+        });
       });
   });
 });
