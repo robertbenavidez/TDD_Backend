a903afa9a2122274da45bb490d9d6a5a14e40b63
Robert Benavidez <murabitbenavidez@gmail.com>
2022-03-07 14:53:22 -0500

    updated linter to ecma 2018, cleaned up the user object

--- src/app.js
+++ src/app.js
@@ -8,11 +8,12 @@ app.use(express.json());
 // create post handler
 app.post('/api/1.0/users', (req, res) => {
   bcrypt.hash(req.body.password, 10).then((hash) => {
-    const user = {
-      username: req.body.username,
-      email: req.body.email,
-      password: hash,
-    };
+    const user = { ...req.body, password: hash };
+    // const user = {
+    //   username: req.body.username,
+    //   email: req.body.email,
+    //   password: hash,
+    // };
     User.create(user).then(() => {
       return res.send({ message: 'User created' });
     });
