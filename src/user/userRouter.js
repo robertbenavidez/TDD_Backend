d720799d6e5406f59aa49421152d53ed3883ba4d
Robert Benavidez <murabitbenavidez@gmail.com>
2022-03-07 19:56:19 -0500

    refactored post new user route using async await

--- src/user/userRouter.js
+++ src/user/userRouter.js
@@ -1,19 +1,21 @@
 const express = require('express');
-const User = require('./User')
+const User = require('./User');
 const bcrypt = require('bcrypt');
 const router = express.Router();
 
-router.post('/api/1.0/users', (req, res) => {
-  bcrypt.hash(req.body.password, 10).then((hash) => {
-    const user = { ...req.body, password: hash };
-    // const user = {
-    //   username: req.body.username,
-    //   email: req.body.email,
-    //   password: hash,
-    // };
-    User.create(user).then(() => {
-      return res.send({ message: 'User created' });
-    });
-  });
+router.post('/api/1.0/users', async (req, res) => {
+  const hash = await bcrypt.hash(req.body.password, 10);
+  const user = await { ...req.body, password: hash };
+  await User.create(user);
+  return res.send({ message: 'User created' });
 });
+//             Promise version
+// router.post('/api/1.0/users', (req, res) => {
+//   bcrypt.hash(req.body.password, 10).then((hash) => {
+//     const user = { ...req.body, password: hash };
+//     User.create(user).then(() => {
+//       return res.send({ message: 'User created' });
+//     });
+//   });
+// });
 module.exports = router;
